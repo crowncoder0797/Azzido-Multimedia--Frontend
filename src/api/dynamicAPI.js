f91b5fe7e832a63240b1d8db04ed6514a75017df
crowncoder0797 <areejsz124@gmail.com>
2023-11-13 13:48:27 +0900

    update api url

--- src/api/dynamicAPI.js
+++ src/api/dynamicAPI.js
@@ -1,27 +1,28 @@
 import axios from 'axios'
 import { config } from '../utils/customLocalStorage'
+import { apiUrl } from '../constants'
 
 const dynamicAPI = async (method, url, obj = {}) => {
   try {
     switch (method) {
       case 'get':
         return await axios
-          .get(`${process.env.REACT_APP_BACKEND_URL}${url}`, config())
+          .get(`${apiUrl}${url}`, config())
           .then((res) => res.data)
 
       case 'post':
         return await axios
-          .post(`${process.env.REACT_APP_BACKEND_URL}${url}`, obj, config())
+          .post(`${apiUrl}${url}`, obj, config())
           .then((res) => res.data)
 
       case 'put':
         return await axios
-          .put(`${process.env.REACT_APP_BACKEND_URL}${url}`, obj, config())
+          .put(`${apiUrl}${url}`, obj, config())
           .then((res) => res.data)
 
       case 'delete':
         return await axios
-          .delete(`${process.env.REACT_APP_BACKEND_URL}${url}`, config())
+          .delete(`${apiUrl}${url}`, config())
           .then((res) => res.data)
     }
   } catch (error) {
